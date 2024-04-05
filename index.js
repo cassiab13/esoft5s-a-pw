73443999486ce42bd6e6b0c2baea6991182412b1
Cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2024-04-05 00:12:12 -0300

    Atividade 04-04

--- index.js
+++ index.js
@@ -2,22 +2,28 @@ const params = new URLSearchParams(location.search);
 
 let namePokemon = params.get('name');
 
-const evolucao = document.querySelector('div');
-console.log(evolucao);
-evolucao.textContent = "Página do " + namePokemon;
+const evolucao = document.querySelector('title');
+evolucao.textContent = `Página do ${namePokemon}`;
+evolucao.setAttribute('aria-label', `Página do ${namePokemon}`);
 
-const p = document.querySelector('p');
-p.textContent = `Evolução do ${namePokemon}`;
+const h1 = document.querySelector('h1');
+const capitalizedPokemonName = namePokemon.charAt(0).toUpperCase() + namePokemon.slice(1);
+h1.textContent = capitalizedPokemonName;
 
-function getPokemon(){
-    const response = fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`).then(data => {
-        return data.json()}).then((data) => {
-            const img = data.sprites.front_default;
-            let imgDiv = document.querySelector('img');
-            imgDiv.src = img;
-            imgDiv.appendChild(img);
-        })
-        return imgDiv;
+const h2 = document.querySelector('h2');
+h2.textContent = `Informações do ${namePokemon}`;
+
+async function getPokemon(){
+     const imgDiv = document.querySelector('img');
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`);
+        const data = await response.json();
+        const img = data.sprites.front_default;
+        imgDiv.src = img;
+        imgDiv.setAttribute('aria-label', `${namePokemon}`);
+    } catch (error) {
+        console.error('Erro ao obter dados do Pokémon:', error);
+    }
 } 
 
-getPokemon()
+getPokemon();
